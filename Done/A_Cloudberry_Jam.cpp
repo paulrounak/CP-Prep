da9da695c609c1abc861e95d013b71b88642b0a0
duckyfied <rounakpaul007@gmail.com>
2025-05-14 05:39:16 +0530

    Update Problems

---
+++ Done/A_Cloudberry_Jam.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n * 2 << endl;
+}
+
+int main () {
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
